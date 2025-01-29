2e6b2e33528c3fc74e25d53891368a4ffceba149
nzyoka10 <mnzyokaeric@gmail.com>
2025-01-29 10:10:48 +0300

    updated readme

---
+++ app.py
@@ -0,0 +1,14 @@
+
+
+
+day = 'Friday'
+
+# greetings function
+def greetings(day):
+    if day == 'Monday':
+        return f"It's {day}, have a great day!"
+    else:
+        return f"It's {day}, have a fruitful week ahead!"
+
+# Call the function
+print(greetings(day))
